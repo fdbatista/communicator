d5106e5f9fecfcdcde5af5ba9edf680ae558b9ca
nico <nico@aleph.engineering>
2021-02-01 02:17:45 -0500

    Set default language

--- config/web.php
+++ config/web.php
@@ -5,6 +5,7 @@ $db = require __DIR__ . '/db.php';
 
 $config = [
     'id' => 'basic',
+    'language' => 'es-MX',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
     'aliases' => [
