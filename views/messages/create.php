434a245f97f508d3cfae1781e64831924c2309d9
nico <nico@aleph.engineering>
2021-02-05 18:08:48 -0500

    Remove button from create.php

--- views/messages/create.php
+++ views/messages/create.php
@@ -1,7 +1,5 @@
 <?php
 
-use yii\helpers\Html;
-
 /* @var $this yii\web\View */
 /* @var $model app\models\entities\Message */
 
@@ -10,8 +8,6 @@ $this->params['breadcrumbs'][] = ['label' => 'Mensajes', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="message-create">
-
-    <h1><?= Html::encode($this->title) ?></h1>
 
     <?= $this->render('_form', [
         'model' => $model,
