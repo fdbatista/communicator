3f3bbf5c65485c2d8ab1689625eb123e0cfe8097
nico <nico@aleph.engineering>
2021-02-04 23:49:00 -0500

    Load env vars via dotEnv library

--- config/db.php
+++ config/db.php
@@ -2,13 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=db;dbname=' . env('DB_DATABASE'),
+    'username' => env('DB_USER'),
+    'password' => env('DB_PASSWORD'),
     'charset' => 'utf8',
-
-    // Schema cache options (for production environment)
-    //'enableSchemaCache' => true,
-    //'schemaCacheDuration' => 60,
-    //'schemaCache' => 'cache',
 ];
