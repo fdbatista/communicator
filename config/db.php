32e79b0dabe94f9d7ae448160134a7c638e38101
nico <nico@aleph.engineering>
2021-02-06 03:08:51 -0500

    Load DB host from env

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=db;dbname=' . env('DB_DATABASE'),
+    'dsn' => 'mysql:host='. env('DB_HOST') . ';dbname=' . env('DB_DATABASE'),
     'username' => env('DB_USER'),
     'password' => env('DB_PASSWORD'),
     'charset' => 'utf8',
