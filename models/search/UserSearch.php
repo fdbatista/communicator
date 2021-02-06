c400f2029168cf4f0772a47f315c6d6667ddfc28
nico <nico@aleph.engineering>
2021-02-05 19:21:21 -0500

    Create user management scaffold

---
+++ models/search/UserSearch.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace app\models\search;
+
+use app\models\entities\User;
+use yii\base\Model;
+use yii\data\ActiveDataProvider;
+
+/**
+ * UserSearch represents the model behind the search form of `app\models\entities\User`.
+ */
+class UserSearch extends User
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['id'], 'integer'],
+            [['user_name', 'full_name', 'password', 'auth_token', 'mobile_number', 'email'], 'safe'],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function scenarios()
+    {
+        // bypass scenarios() implementation in the parent class
+        return Model::scenarios();
+    }
+
+    /**
+     * Creates data provider instance with search query applied
+     *
+     * @param array $params
+     *
+     * @return ActiveDataProvider
+     */
+    public function search($params)
+    {
+        $query = User::find();
+
+        // add conditions that should always apply here
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+        ]);
+
+        $this->load($params);
+
+        if (!$this->validate()) {
+            // uncomment the following line if you do not want to return any records when validation fails
+            // $query->where('0=1');
+            return $dataProvider;
+        }
+
+        // grid filtering conditions
+        $query->andFilterWhere([
+            'id' => $this->id,
+        ]);
+
+        $query->andFilterWhere(['like', 'user_name', $this->user_name])
+            ->andFilterWhere(['like', 'full_name', $this->full_name])
+            ->andFilterWhere(['like', 'password', $this->password])
+            ->andFilterWhere(['like', 'auth_token', $this->auth_token])
+            ->andFilterWhere(['like', 'mobile_number', $this->mobile_number])
+            ->andFilterWhere(['like', 'email', $this->email]);
+
+        return $dataProvider;
+    }
+}
---
+++ views/user/_form.php
@@ -0,0 +1,33 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\entities\User */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="user-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'user_name')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'full_name')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'password')->passwordInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'auth_token')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'mobile_number')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'email')->textInput(['maxlength' => true]) ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
